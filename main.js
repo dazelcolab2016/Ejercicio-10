ac3fe0e1006af562ede98c31a55c2564ebc621a9
Zeldina Chalco <dazelcolab2016@gmail.com>
2016-12-27 16:44:13 -0500

    area master jedi

--- main.js
+++ main.js
@@ -9,14 +9,14 @@ function evaluar()
         diez.innerHTML = suma;
 
 	if(a ==10 || b == 10 || suma == 10 )
-	{
-		make10 = 1;
-		salida.innerHTML = make10;
-	}else{
-		make10 = 0;
-        salida.innerHTML = make10;
-	}
-}
+    {
+            make10 = 1;
+            salida.innerHTML = make10;
+        }else{
+            make10 = 0;
+            salida.innerHTML = make10;
+        }
+    }
 
 
 
